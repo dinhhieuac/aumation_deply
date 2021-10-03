05bfc2553b50763e039a9988ed9998a76e8236d9
hieuhd <dinhhieuac@gmail.com>
2021-10-03 17:52:59 +0700

    Update pool1.sh

--- pool1.sh
+++ pool1.sh
@@ -1,6 +1,6 @@
-sudo apt-get install linux-headers-$(uname -r) -y 
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g') 
-wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin 
+sudo apt-get install linux-headers-$(uname -r) -y
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
 sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
 echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
@@ -9,9 +9,7 @@ sudo apt-get -y install cuda-drivers
 sudo apt-get install libcurl3 -y
 woker=$USER
 woker+=_$(date +'%d%m_%H%M%S')
-wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
-tar -zxvf t-rex-0.21.6-linux.tar.gz
-sudo killall XXX
-sudo screen ./t-rex -a ethash -o us-eth.2miners.com:2020 -u 0xfbbaaec0813a4bb8420b956f4c80519cdabbeb9c -p x -w $woker
-echo $woker &
-
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+cd bin
+./ethminer -U -P stratum://0xfbbaaec0813a4bb8420b956f4c80519cdabbeb9c.$woker@us2.ethermine.org:4444 &
